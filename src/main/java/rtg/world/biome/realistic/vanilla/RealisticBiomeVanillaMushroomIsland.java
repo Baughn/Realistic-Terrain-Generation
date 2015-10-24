fda46fc848b3219d93846b93fd48ba940914a2ae
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-24 22:26:39 +0100

    Initial proof-of-concept to force Mushroom Islands to generate as islands.

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaMushroomIsland.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaMushroomIsland.java
@@ -1,29 +1,36 @@
 package rtg.world.biome.realistic.vanilla;
 
+import java.util.Random;
+
 import rtg.config.vanilla.ConfigVanilla;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
 import rtg.world.gen.surface.vanilla.SurfaceVanillaMushroomIsland;
 import rtg.world.gen.terrain.vanilla.TerrainVanillaMushroomIsland;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class RealisticBiomeVanillaMushroomIsland extends RealisticBiomeVanillaBase
-{	
-	public static Block topBlock = BiomeGenBase.mushroomIsland.topBlock;
-	public static Block fillerBlock = BiomeGenBase.mushroomIsland.fillerBlock;
-	
-	public RealisticBiomeVanillaMushroomIsland()
-	{
-		super(
-			BiomeGenBase.mushroomIsland,
-			BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
-			new TerrainVanillaMushroomIsland(),
-			new SurfaceVanillaMushroomIsland(topBlock, fillerBlock, 67, topBlock, 0f)
-		);
-		
-		this.setRealisticBiomeName("Vanilla Mushroom Island");
-		this.biomeCategory = BiomeCategory.WET;
-		this.biomeWeight = ConfigVanilla.weightVanillaMushroomIsland;
-	}	
+{
+    
+    public static Block topBlock = Blocks.mycelium;
+    public static Block fillerBlock = Blocks.mycelium;
+    
+    public RealisticBiomeVanillaMushroomIsland()
+    {
+    
+        super(
+            BiomeGenBase.mushroomIsland,
+            BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
+            new TerrainVanillaMushroomIsland(),
+            new SurfaceVanillaMushroomIsland(Blocks.gravel, Blocks.gravel, Blocks.clay, 20f, 0.1f));
+        
+        this.setRealisticBiomeName("Vanilla Mushroom Island");
+        this.biomeCategory = BiomeCategory.WET;
+        this.biomeWeight = ConfigVanilla.weightVanillaMushroomIsland;
+    }
 }
--- src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaMushroomIsland.java
+++ src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaMushroomIsland.java
@@ -3,9 +3,9 @@ package rtg.world.gen.surface.vanilla;
 import java.util.Random;
 
 import rtg.util.CellNoise;
-import rtg.util.CliffCalculator;
 import rtg.util.OpenSimplexNoise;
 import rtg.world.gen.surface.SurfaceBase;
+
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
@@ -13,109 +13,61 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public class SurfaceVanillaMushroomIsland extends SurfaceBase
 {
-	private int beach;
-	private Block beachBlock;
-	private float min;
-	
-	private float sCliff = 1.5f;
-	private float sHeight = 60f;
-	private float sStrength = 65f;
-	private float cCliff = 1.5f;
-	
-	public SurfaceVanillaMushroomIsland(Block top, Block fill, int beachHeight, Block genBeachBlock, float minCliff) 
-	{
-		super(top, fill);
-		beach = beachHeight;
-		beachBlock = genBeachBlock;
-		min = minCliff;
-	}
-	
-	public SurfaceVanillaMushroomIsland(Block top, Block fill, int beachHeight, Block genBeachBlock, float minCliff, float stoneCliff, float stoneHeight, float stoneStrength, float clayCliff)
-	{
-		this(top, fill, beachHeight, genBeachBlock, minCliff);
-		
-		sCliff = stoneCliff;
-		sHeight = stoneHeight;
-		sStrength = stoneStrength;
-		cCliff = clayCliff;
-	}
-	
-	@Override
-	public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand, OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
-	{
-		float c = CliffCalculator.calc(x, y, noise);
-		int cliff = 0;
-		boolean gravel = false;
-		
-    	Block b;
-		for(int k = 255; k > -1; k--)
-		{
-			b = blocks[(y * 16 + x) * 256 + k];
-            if(b == Blocks.air)
+    
+    private Block mix1Block;
+    private float width;
+    private float height;
+    private float mixCheck;
+    
+    public SurfaceVanillaMushroomIsland(Block top, Block filler, Block mix1, float mixWidth, float mixHeight)
+    {
+    
+        super(top, filler);
+        
+        mix1Block = mix1;
+        
+        width = mixWidth;
+        height = mixHeight;
+    }
+    
+    @Override
+    public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand,
+        OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
+    {
+    
+        
+        for (int k = 255; k > -1; k--)
+        {
+            Block b = blocks[(y * 16 + x) * 256 + k];
+            if (b == Blocks.air)
             {
-            	depth = -1;
+                depth = -1;
             }
-            else if(b == Blocks.stone)
+            else if (b == Blocks.stone)
             {
-            	depth++;
-            	
-            	if(depth == 0)
-            	{
-            		if(k < beach)
-            		{
-            			gravel = true;
-            		}
-
-					float p = simplex.noise3(i / 8f, j / 8f, k / 8f) * 0.5f;
-        			if(c > min && c > sCliff - ((k - sHeight) / sStrength) + p)
-        			{
-        				cliff = 1;
-        			}
-            		if(c > cCliff)
-        			{
-        				cliff = 2;
-        			}
-            		
-            		if(cliff == 1)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = rand.nextInt(3) == 0 ? Blocks.cobblestone : Blocks.stone; 
-            		}
-            		else if(cliff == 2)
-            		{
-        				blocks[(y * 16 + x) * 256 + k] = Blocks.stained_hardened_clay; 
-        				metadata[(y * 16 + x) * 256 + k] = 9; 
-            		}
-            		else if(k < beach)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = beachBlock;
-            			gravel = true;
-            		}
-            		else
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = topBlock;
-            		}
-            	}
-            	else if(depth < 6)
-        		{
-            		if(cliff == 1)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = Blocks.stone; 
-            		}
-            		else if(cliff == 2)
-            		{
-        				blocks[(y * 16 + x) * 256 + k] = Blocks.stained_hardened_clay; 
-        				metadata[(y * 16 + x) * 256 + k] = 9; 
-            		}
-            		else if(gravel)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = beachBlock;
-            		}
-            		else
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = fillerBlock;
-            		}
-        		}
+                depth++;
+                
+                if (depth == 0 && k > 0 && k < 63)
+                {
+                    mixCheck = simplex.noise2(i / width, j / width);
+                    
+                    if (mixCheck > height) {
+                        blocks[(y * 16 + x) * 256 + k] = mix1Block;
+                    }
+                    else
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = topBlock;
+                    }
+                }
+                else if (depth < 4 && k < 63)
+                {
+                    blocks[(y * 16 + x) * 256 + k] = fillerBlock;
+                }
+                else {
+                    blocks[(y * 16 + x) * 256 + k] = Blocks.mycelium;
+                }
+                   
             }
-		}
-	}
+        }
+    }
 }
